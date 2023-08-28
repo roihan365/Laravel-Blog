bf48685b7ad6b1bbefa1ba565d3cca9b0b180b13
roihan365 <bitcoiner.banjarmasin@gmail.com>
2023-08-28 09:35:59 +0800

    admin page

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -17,6 +18,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Route::get('/admin', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/user', function(){
+    return view('admin.login');
+})->name('login');
 
 Auth::routes();
